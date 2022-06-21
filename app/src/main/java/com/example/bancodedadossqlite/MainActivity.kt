c31c374720aaa8066741fb7c0a816620c3a522bb
erickferrareto <e.ferraretowork@gmail.com>
2022-06-20 23:01:37 -0300

    First Commit

---
+++ app/src/main/java/com/example/bancodedadossqlite/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.bancodedadossqlite
+
+import android.database.sqlite.SQLiteDatabase
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bancodedadossqlite.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var biding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        biding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(biding.root)
+        biding.TXThello.text = "hi Erick"
+
+    }
+}
