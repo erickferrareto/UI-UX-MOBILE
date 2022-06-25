ddff3ce9f7c202e77447179f372d57fff9eb3083
erickferrareto <e.ferraretowork@gmail.com>
2022-06-25 19:51:38 -0300

    commit

---
+++ app/src/main/java/com/example/bancodedadossqlite/DetailActivity.kt
@@ -0,0 +1,20 @@
+package com.example.bancodedadossqlite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.bancodedadossqlite.databinding.ActivityDetailBinding
+import com.example.bancodedadossqlite.databinding.ActivityMainBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    lateinit private var biding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        biding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(biding.root)
+        setSupportActionBar(biding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
